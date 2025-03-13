37be8f81f5568093e1912496d740339f24fffc00
OmarEleno <l23170284@Culiacan.tecnm.mx>
2025-03-13 22:47:42 +0000

    final

--- sketch.js
+++ sketch.js
@@ -10,12 +10,18 @@ let wallY = [];
 let pared;
 let puntaje = 0;
 let puntajeMax = 0;
+let recordAnterior = 0;
+let musicaRecord;
+let musicaJuego;
+
 function preload() {
   // put preload code here
   imagenFondo = loadImage('./images/fondojuego00.png');
   imagenInicio = loadImage('./images/fondoInicio.jpg');
   personaje = loadImage('./images/bird.png');
   pared = loadImage('./images/pared.png');
+  musicaRecord = loadSound('./sounds/aplauso.wav')
+  musicaJuego = loadSound('./sounds/musicafondo.mp3')
 }
 
 function setup() {
@@ -50,20 +56,33 @@ function draw() {
     if(wallX[i] === 100){
       puntaje = puntaje + 1;
       puntajeMax = max(puntaje, puntajeMax);
-      wallX[i] = wallX[i] - 5;
     }
+        wallX[i] = wallX[i] - 5;
+          if(posY < -60 || posY > height+60  
+            || (abs(wallX[i]-100)<60 
+            && abs(wallY[i]-posY)>100)){
+            musicaJuego.stop();
+            estado=0;
+          } 
     }
 
     //Personaje
   image(personaje, 100, posY, 60, 60); 
   text("Puntaje: " + puntaje, width/2, 100);
 } else if(estado === 0){
+  background(0);
+  imageMode(CORNER);
   cursor();
   image(imagenInicio, 0, 0, 450, 600);
   textSize(24);
   fill(255);
-  text("Haga clic para comenzar", 600, 100);
-  text("Haga clic para comenzar", 700, 100);
+  text("Puntaje Maximo: "+puntajeMax, 600, 100);
+  text("Haga clic para comenzar", 600, 200);
+  if(puntajeMax > recordAnterior){
+    if(!musicaRecord.isPlaying()){
+    musicaRecord.play();
+    }
+  }
 }
 }
 
@@ -73,12 +92,18 @@ function mousePressed(){
     posY = 100;
     x = 0;
     dY = 3;
-    wallX = [500, 800, 110];
+    wallX = [500, 800, 1100];
     wallY [0] = random(200, 300);
     wallY [1] = random(200, 300);
     wallY [2] = random(200, 300);
-
+    puntaje = 0;
+    recordAnterior = puntajeMax;
     noCursor();
+
+    if(musicaRecord.isPlaying()){
+      musicaRecord.stop();
+    }
+    musicaJuego.loop();
   }
   dY = -15;
 
